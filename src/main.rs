37ff7b3796ca5c544715870ddc04d359ef8a70d0
Michael Killough <michael.killough@deliveroo.co.uk>
2018-12-30 15:20:40 +0000

    Move into struct.

--- src/main.rs
+++ src/main.rs
@@ -18,125 +18,105 @@ struct NodeBucket {
     duration: EdgeWeight,
 }
 
-fn get_loop_weight(graph: &Graph, node: NodeId, use_duration: bool) -> Option<EdgeWeight> {
-    let mut loop_weight = None;
-    for edge in graph.get_adjacent_edges(node, Direction::Forward) {
-        if node == edge.target {
-            let value = if use_duration {
-                edge.duration
-            } else {
-                edge.weight
-            };
-            loop_weight = loop_weight
-                .map(|weight| EdgeWeight::min(weight, value))
-                .or(Some(value));
-        }
-    }
-    loop_weight
+struct ManyToMany {
+    source_queries: Vec<Vec<Query>>,
+    target_queries: Vec<Vec<Query>>,
+    num_sources: usize,
+    num_targets: usize,
+
+    results: Vec<Option<(EdgeWeight, EdgeWeight)>>,
+
+    graph: Graph,
+    buckets: Vec<NodeBucket>,
+    heap: QueryHeap,
 }
 
-fn should_add_loop_weight(
-    graph: &Graph,
-    node: NodeId,
-    weight: EdgeWeight,
-    duration: EdgeWeight,
-) -> Option<(EdgeWeight, EdgeWeight)> {
-    // Special case for CH when contractor creates a loop edge node->node.
-    assert!(weight < 0);
+impl ManyToMany {
+    fn new() -> Result<ManyToMany> {
+        let target_queries = vec![vec![
+            Query {
+                node: 861677,
+                weight: 77,
+                parent: 861677,
+                duration: 77,
+            },
+            Query {
+                node: 861680,
+                weight: 29,
+                parent: 861680,
+                duration: 29,
+            },
+        ]];
+        let source_queries = vec![vec![
+            Query {
+                node: 791407,
+                weight: -477,
+                parent: 791407,
+                duration: 477,
+            },
+            Query {
+                node: 791413,
+                weight: -87,
+                parent: 791413,
+                duration: 87,
+            },
+        ]];
+        let num_sources = 1;
+        let num_targets = 1;
 
-    if let Some(loop_weight) = get_loop_weight(graph, node, false) {
-        let new_weight_with_loop = weight + loop_weight;
-        if new_weight_with_loop >= 0 {
-            let loop_duration = get_loop_weight(graph, node, true).unwrap();
-            let new_duration_with_loop = duration + loop_duration;
-            return Some((new_weight_with_loop, new_duration_with_loop));
-        }
+        let results = vec![None; num_sources * num_targets];
+
+        let buckets = Vec::new();
+        let heap = QueryHeap::new();
+        let graph = Graph::from_file("data/1.osrm.hsgr")?;
+
+        Ok(ManyToMany {
+            source_queries,
+            target_queries,
+            num_sources,
+            num_targets,
+            results,
+            buckets,
+            graph,
+            heap,
+        })
     }
 
-    // No loop found or adjusted weight is negative.
-    return None;
-}
+    fn perform(&mut self) {
+        for target_idx in 0..self.target_queries.len() {
+            for query in &self.target_queries[target_idx] {
+                self.heap.push(*query);
+            }
 
-fn stall_at_node(
-    graph: &Graph,
-    heap: &mut QueryHeap,
-    direction: Direction,
-    node: NodeId,
-    weight: EdgeWeight,
-) -> bool {
-    for edge in graph.get_adjacent_edges(node, !direction) {
-        if let Some(query) = heap.get(edge.target) {
-            if query.weight + edge.weight < weight {
-                return true;
+            while let Some(query) = self.heap.pop() {
+                self.backward_search(query, target_idx);
+            }
+        }
+
+        self.buckets.sort_by_key(|bucket| bucket.middle_node);
+
+        self.heap = QueryHeap::new();
+        for source_idx in 0..self.source_queries.len() {
+            for query in &self.source_queries[source_idx] {
+                self.heap.push(*query);
+            }
+
+            while let Some(query) = self.heap.pop() {
+                self.forward_search(query, source_idx);
             }
         }
     }
-    return false;
-}
 
-fn relax_outgoing_edges(
-    graph: &Graph,
-    heap: &mut QueryHeap,
-    direction: Direction,
-    node: NodeId,
-    weight: EdgeWeight,
-    duration: EdgeWeight,
-) {
-    if stall_at_node(graph, heap, direction, node, weight) {
-        return;
-    }
-
-    for edge in graph.get_adjacent_edges(node, direction) {
-        let query = Query {
-            node: edge.target,
-            parent: node,
-            weight: weight + edge.weight,
-            duration: weight + edge.duration,
-        };
-
-        if let Some(current) = heap.get(edge.target) {
-            if (query.weight, query.duration) < (current.weight, current.duration) {
-                heap.push(query)
-            }
-        } else {
-            heap.push(query)
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    let graph = Graph::from_file("data/1.osrm.hsgr")?;
-
-    let mut heap = QueryHeap::new();
-    heap.push(Query {
-        node: 861677,
-        weight: 77,
-        parent: 861677,
-        duration: 77,
-    });
-    heap.push(Query {
-        node: 861680,
-        weight: 29,
-        parent: 861680,
-        duration: 29,
-    });
-
-    let mut buckets: Vec<NodeBucket> = Vec::new();
-
-    while !heap.is_empty() {
-        let query = heap.pop().unwrap();
-
-        buckets.push(NodeBucket {
+    fn backward_search(&mut self, query: Query, column: usize) {
+        self.buckets.push(NodeBucket {
             middle_node: query.node,
             parent_node: query.parent,
-            column_index: 0, // XXX
+            column_index: column,
             weight: query.weight,
             duration: query.duration,
         });
 
-        relax_outgoing_edges(
-            &graph,
-            &mut heap,
+        self.relax_outgoing_edges(
             Direction::Backward,
             query.node,
             query.weight,
@@ -144,45 +124,19 @@ fn main() -> Result<()> {
         );
     }
 
-    println!("Number of buckets: {}", buckets.len());
-
-    buckets.sort_by_key(|bucket| bucket.middle_node);
-
-    let number_of_sources = 1;
-    let number_of_targets = 1;
-    let number_of_entries = number_of_sources * number_of_targets;
-
-    let mut results_table: Vec<Option<(EdgeWeight, EdgeWeight)>> = vec![None; number_of_entries];
-
-    let row_index = 0;
-    let column_index = 0;
-
-    let mut heap = QueryHeap::new();
-    heap.push(Query {
-        node: 791407,
-        weight: -477,
-        parent: 791407,
-        duration: 477,
-    });
-    heap.push(Query {
-        node: 791413,
-        weight: -87,
-        parent: 791413,
-        duration: 87,
-    });
-    while !heap.is_empty() {
-        let query = heap.pop().unwrap();
-
+    fn forward_search(&mut self, query: Query, row: usize) {
         let source_weight = query.weight;
         let source_duration = query.duration;
 
-        let range = buckets.equal_range_by_key(&query.node, |bucket| bucket.middle_node);
-        for bucket in &buckets[range] {
+        let range = self
+            .buckets
+            .equal_range_by_key(&query.node, |bucket| bucket.middle_node);
+        for bucket in &self.buckets[range] {
             let target_weight = bucket.weight;
             let target_duration = bucket.duration;
 
-            let idx = row_index * number_of_entries + column_index;
-            let current = results_table[idx];
+            let idx = row * self.num_targets + bucket.column_index;
+            let current = self.results[idx];
 
             let new_weight = source_weight + target_weight;
             let new_duration = source_duration + target_duration;
@@ -190,33 +144,108 @@ fn main() -> Result<()> {
 
             if new_weight < 0 {
                 if let Some((loop_weight, loop_duration)) =
-                    should_add_loop_weight(&graph, query.node, new_weight, new_duration)
+                    self.should_add_loop_weight(query.node, new_weight, new_duration)
                 {
                     if Some((loop_weight, loop_duration)) < current {
-                        results_table[idx] = Some((loop_weight, loop_duration));
+                        self.results[idx] = Some((loop_weight, loop_duration));
                     }
                 }
             } else if current.is_none() {
-                results_table[idx] = Some(new);
+                self.results[idx] = Some(new);
             } else if let Some(current) = current {
                 if new < current {
-                    results_table[idx] = Some(new);
+                    self.results[idx] = Some(new);
                 }
             }
         }
 
-        relax_outgoing_edges(
-            &graph,
-            &mut heap,
-            Direction::Forward,
-            query.node,
-            query.weight,
-            query.duration,
-        );
+        self.relax_outgoing_edges(Direction::Forward, query.node, query.weight, query.duration);
     }
 
-    println!("Number of buckets: {}", buckets.len());
-    println!("Results: {:?}", results_table);
+    fn stall_at_node(&self, direction: Direction, node: NodeId, weight: EdgeWeight) -> bool {
+        for edge in self.graph.get_adjacent_edges(node, !direction) {
+            if let Some(query) = self.heap.get(edge.target) {
+                if query.weight + edge.weight < weight {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    fn relax_outgoing_edges(
+        &mut self,
+        direction: Direction,
+        node: NodeId,
+        weight: EdgeWeight,
+        duration: EdgeWeight,
+    ) {
+        if self.stall_at_node(direction, node, weight) {
+            return;
+        }
+
+        for edge in self.graph.get_adjacent_edges(node, direction) {
+            let query = Query {
+                node: edge.target,
+                parent: node,
+                weight: weight + edge.weight,
+                duration: weight + edge.duration,
+            };
+
+            if let Some(current) = self.heap.get(edge.target) {
+                if (query.weight, query.duration) < (current.weight, current.duration) {
+                    self.heap.push(query)
+                }
+            } else {
+                self.heap.push(query)
+            }
+        }
+    }
+
+    fn get_loop_weight(&self, node: NodeId, use_duration: bool) -> Option<EdgeWeight> {
+        let mut loop_weight = None;
+        for edge in self.graph.get_adjacent_edges(node, Direction::Forward) {
+            if node == edge.target {
+                let value = if use_duration {
+                    edge.duration
+                } else {
+                    edge.weight
+                };
+                loop_weight = loop_weight
+                    .map(|weight| EdgeWeight::min(weight, value))
+                    .or(Some(value));
+            }
+        }
+        loop_weight
+    }
+
+    fn should_add_loop_weight(
+        &self,
+        node: NodeId,
+        weight: EdgeWeight,
+        duration: EdgeWeight,
+    ) -> Option<(EdgeWeight, EdgeWeight)> {
+        // Special case for CH when contractor creates a loop edge node->node.
+        assert!(weight < 0);
+
+        if let Some(loop_weight) = self.get_loop_weight(node, false) {
+            let new_weight_with_loop = weight + loop_weight;
+            if new_weight_with_loop >= 0 {
+                let loop_duration = self.get_loop_weight(node, true).unwrap();
+                let new_duration_with_loop = duration + loop_duration;
+                return Some((new_weight_with_loop, new_duration_with_loop));
+            }
+        }
+
+        // No loop found or adjusted weight is negative.
+        return None;
+    }
+}
+
+fn main() -> Result<()> {
+    let mut m2m = ManyToMany::new()?;
+    m2m.perform();
+    println!("Results: {:?}", m2m.results);
 
     Ok(())
 }
