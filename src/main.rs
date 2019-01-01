21eec6098c0f161fa4080ad31556d243793978ae
Michael Killough <michael.killough@deliveroo.co.uk>
2019-01-01 17:00:23 +0000

    Compare results to check we're working. (We are!)

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
-use serde::Deserialize;
+use serde::{de::DeserializeOwned, Deserialize};
 use serde_json;
 
 pub use self::errors::*;
@@ -20,10 +20,13 @@ struct Queries {
     targets: Vec<Vec<heap::Query>>,
 }
 
-fn load_queries(path: impl AsRef<Path>) -> Result<Queries> {
+fn load_json<T>(path: impl AsRef<Path>) -> Result<T>
+where
+    T: DeserializeOwned,
+{
     let file = BufReader::new(File::open(path)?);
-    let queries = serde_json::from_reader(file)?;
-    Ok(queries)
+    let deserialized = serde_json::from_reader(file)?;
+    Ok(deserialized)
 }
 
 fn time<F, R>(name: &str, f: F) -> R
@@ -38,14 +41,28 @@ where
 }
 
 fn main() -> Result<()> {
-    let queries = load_queries("data/queries.json")?;
+    let queries: Queries = load_json("data/queries.json")?;
+    let num_targets = queries.targets.len();
+    let expected_results: Vec<Vec<f64>> = load_json("data/results.json")?;
     let graph = Graph::from_file("data/1.osrm.hsgr")?;
 
     let mut m2m = ManyToMany::new(graph, queries.targets, queries.sources)?;
 
     time("m2m.perform()", || m2m.perform());
 
-    println!("Results: {:?}", m2m.results.len());
+    let results: Vec<Vec<f64>> = m2m
+        .results
+        .chunks(num_targets)
+        .map(|row| {
+            row.iter()
+                .map(|option| option.unwrap_or((0, 0)))
+                // Durations are in deciseconds (why??)
+                .map(|(_, duration)| duration as f64 / 10.)
+                .collect()
+        })
+        .collect();
+
+    println!("equal? {}", results == expected_results);
 
     Ok(())
 }
